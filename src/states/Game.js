c426ad71a9abb0581ce20776b86aa4db317dbbe7
robinre <robin.reicher@hiq.se>
2016-12-14 16:35:01 +0100

    WOHO!

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,52 +1,19 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
+import Piece from '../piece'
 import {setResponsiveWidth} from '../utils'
 
 export default class extends Phaser.State {
   init () {}
   preload () {}
 
-  buildGrid() {
-    this.graphics = this.game.add.graphics(0, 0);
-    this.graphics.lineStyle(2, 0xFFFFFF, 0.15);
-
-    var margin = 30;
-    var size = 20;
-
-    var width = this.game.width - (margin * 2);
-    var cellSize = width / size;
-    var maxHeight = this.game.height - (margin * 2);
-    var yCells = Math.floor(maxHeight / cellSize);
-    var height = yCells * cellSize;
-
-    for (var x = 0; x <= width; x += cellSize){
-      this.graphics.moveTo(margin + x, margin);
-      this.graphics.lineTo(margin + x, margin + height);
-    }
-
-    for (var y = 0; y <= height; y += cellSize){
-      this.graphics.moveTo(margin, margin + y);
-      this.graphics.lineTo(margin + width, margin + y );
-    }
-
-    //beutiful
+  create (){
+    var startPoint = new Phaser.Point(this.game.world.centerX, 400);
+    var currentPiece = new Piece(this.game, startPoint.x, startPoint.y, [new Phaser.Point(0, -100), new Phaser.Point(100, 0), new Phaser.Point(0, -100)]);
+    this.game.add.existing(currentPiece);
   }
 
-  create () {
-    this.buildGrid();
+  update (){
 
-  }
-
-  update () {
-
-  }
-
-  render () {
-
-    if (__DEV__) {
-      //this.game.debug.spriteInfo(this.mushroom, 32, 32)
-      //this.game.debug.lineInfo(line1, 32, 32);
-    }
   }
 }
